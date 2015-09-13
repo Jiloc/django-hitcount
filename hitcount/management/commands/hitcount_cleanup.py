3bc2431d9a2ebaec8d8a875c92a92a73e5139efc
Francesco Leacche <francescoleacche@gmail.com>
2015-09-13 22:41:47 +0200

    fix hitcount cleanup for django 1.8

--- hitcount/management/commands/hitcount_cleanup.py
+++ hitcount/management/commands/hitcount_cleanup.py
@@ -1,15 +1,22 @@
-import datetime
-from django.utils.timezone import now
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import timedelta
+
+from django.conf import settings
+from django.utils import timezone
 from django.core.management.base import NoArgsCommand
+
+from hitcount.models import Hit
+
 
 class Command(NoArgsCommand):
     help = "Can be run as a cronjob or directly to clean out old Hits objects from the database."
 
     def handle_noargs(self, **options):
-        from django.db import transaction
-        from hitcount.models import Hit
-        from django.conf import settings
-        grace = getattr(settings, 'HITCOUNT_KEEP_HIT_IN_DATABASE', {'days':30})
-        period = now() - datetime.timedelta(**grace)
-        Hit.objects.filter(created__lt=period).delete()
-        transaction.commit_unless_managed()
+        grace = getattr(settings, 'HITCOUNT_KEEP_HIT_IN_DATABASE', {'days': 30})
+        period = timezone.now() - timedelta(**grace)
+        qs = Hit.objects.filter(created__lt=period)
+        number_removed = len(qs)
+        qs.delete()
+        self.stdout.write('Successfully removed %s Hits' % number_removed)
